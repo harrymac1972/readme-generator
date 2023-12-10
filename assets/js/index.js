af39c8cfdcfe103d64a9d60db345900b04d7199f
harry <harrymac1972@gmail.com>
2023-12-10 04:29:53 -0700

    base input questions done

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,37 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
+// test instructions
 const questions = [
     {
       type: 'input',
       name: 'title',
-      message: 'What is the Project Title?',
+      message: 'Project Title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Description?',
+    },
+    {
+      type: 'input',
+      name: 'instructions',
+      message: 'Installation Instructions?',
+    },
+    {
+      type: 'input',
+      name: 'usageInfo',
+      message: 'Usage Information?',
+    },
+    {
+      type: 'input',
+      name: 'guidelines',
+      message: 'Contribution Guidelines?',
+    },
+    {
+      type: 'input',
+      name: 'testing',
+      message: 'Test Instructions?',
     },
 ];
 
