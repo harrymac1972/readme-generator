c6122d8ceb5c3f3926cd43370a72dcf1f7d7b48a
harry <harrymac1972@gmail.com>
2023-12-10 04:03:36 -0700

    base structure started

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,32 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the Project Title?',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    console.log(data);
+    // fs.writeFile(filename,dataStr);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        writeToFile(data);
+    });
+}
 
 // Function call to initialize app
 init();
+
+
