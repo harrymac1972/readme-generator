674d014f07c05504dec3b909b3d675b0852d281a
harry <harrymac1972@gmail.com>
2023-12-13 04:52:57 -0700

    remove commented out lines

--- assets/js/index.js
+++ assets/js/index.js
@@ -44,13 +44,8 @@ const questions = [
       choices: ['Apache License 2.0',
                 'GNU General Public License v3.0',
                 'MIT License',
-                // 'BSD 2-Clause "Simplified" License',
-                // 'BSD 3-Clause "New" or "Revised" License',
                 'Boost Software License 1.0',
-                // 'Creative Commons Zero v1.0 Universal',
-                // 'Eclipse Public License 2.0',
                 'GNU Affero General Public License v3.0',
-                // 'GNU General Public License v2.0',
                 'GNU Lesser General Public License v2.1',
                 'Mozilla Public License 2.0',
                 'The Unlicense'
@@ -60,7 +55,6 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(data) {
-  console.log(data);
 
   // compose Main String
   let mainStr = "";
